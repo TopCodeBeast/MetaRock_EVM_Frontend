4f8e1061eaba917ab4fe79c39ed9b694a90eee92
TopCodeBeast <maxb47163@gmail.com>
2022-04-04 08:56:52 +0900

    temp

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const nftAddress = "0x8894E5818c7612F06615893F901FD7F9D8398783";
-export const marketAddress = "0x42a2C33497F573582aa3DFFA7B1A9e0b888696c6";
+export const marketAddress = "0xA1B6FA902A653968D85e5207BDA819d4f58A7C5A";
